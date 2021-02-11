baa5297a0ae93e6e614d3120b5620144e88b138d
Aditi Saxena <aditi.saxena2307@gmail.com>
2021-02-11 15:39:21 +0530

    Update about_page.php

--- E_Vendor/about_page.php
+++ E_Vendor/about_page.php
@@ -19,7 +19,52 @@
         <?php
         include 'include/header.php';
         ?>
-        
+           <div class='container' >
+            <div class='row'>
+                <!-- who we are column-->
+                <div class='col-xs-12 col-sm-4'>
+                    <div class='panel panel-default' style='border:none;'>
+                        <div class='panel-body'>
+                            <h2>Who we are</h2>
+                            <p style='font-weight:normal; font-style:italic;'>We’re E-Vendor — a global brand with a serious passion for Vendors. 
+                                Everything in our store has 
+                                met an extremely high bar of quality and long-term customer satisfaction. </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- mission column-->
+                <div class='col-xs-12 col-sm-4'>
+                    <div class='panel panel-default' style='border:none;'>
+                        <div class='panel-body'>
+                            <center> <img src='img/1.jpeg' class='image-thumbnail img-responsive' alt='About us' style='border-radius:5px; padding-top:25px;'></center>
+                            <h2>Our Mission</h2>
+                            <p style='font-weight:normal; font-style:italic;'>To facilitate global trade between worldwide buyers and Indian suppliers.</p>
+                            <p style='font-weight:normal; font-style:italic;'>To provide accurate and dependable information on products and suppliers to global buyers.</p>
+                            <p style='font-weight:normal; font-style:italic;'>To help buyers and suppliers communicate and do business with each other effectively and efficiently.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <!-- career column -->
+                <div class='col-xs-12 col-sm-4'>
+                    <div class='panel panel-default' style='border:none;'>
+                        <div class='panel-body'>
+                            <h2>Career</h2>
+                            
+                                <h5>Why Us?<h5>
+                           
+                            <p style='font-weight:normal; font-style:italic;'>
+                                E-Vendor is a place where you have the freedom to focus all your 
+                                innovative energy in the right direction.While we don't believe that a 
+                                professional degree is required, it's extremely helpful if you have a degree
+                                in Business Management, Commerce or Marketing.</p>
+                            <h5>So what's available?</h5>
+                            <p style='font-weight:normal; font-style:italic;'>1. Marketing & Website Intern <br>2. Senior Graphic Designer<br>3. Customer Happiness Officer<br>5. Digital Marketing Operations Manager</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php
         include 'include/footer.php';
         ?>
