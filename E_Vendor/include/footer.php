4cd240708246c1fb6e22f8abb879aab6c1dc5f11
Aditi Saxena <aditi.saxena2307@gmail.com>
2021-02-11 15:17:52 +0530

    Add files via upload

---
+++ E_Vendor/include/footer.php
@@ -0,0 +1,73 @@
+<footer class="nb-footer">
+<div class="container">
+<div class="row">
+<div class="col-sm-12">
+<div class="about">
+  <div class="social-media">
+    <ul class="list-inline">
+      <li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+      <li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
+      <li><a href="#" title=""><i class="fa fa-linkedin"></i></a></li>
+    </ul>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+  <h2 class="title">Help Center</h2>
+  <ul class="list-unstyled">
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> How to Pay</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> FAQ's</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Sitemap</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Delivery Info</a></li>
+  </ul>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+  <h2 class="title">Customer information</h2>
+  <ul class="list-unstyled">
+      <li><a href="./about_page.php" title=""><i class="fa fa-angle-double-right"></i> About Us</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> FAQ's</a></li>
+    <li><a href="./sell_page.php" title=""><i class="fa fa-angle-double-right"></i> Sell Your Items</a></li>
+    <li><a href="./contact_page.php" title=""><i class="fa fa-angle-double-right"></i> Contact Us</a></li>
+  </ul>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+  <h2 class="title">Security & privacy</h2>
+  <ul class="list-unstyled">
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Terms Of Use</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Privacy Policy</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Return / Refund Policy</a></li>
+    <li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Store Locations</a></li>
+  </ul>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+  <h2 class="title">Payment</h2>
+  <p>Sample Payment Method</p>
+  
+</div>
+</div>
+</div>
+</div>
+
+<section class="copyright">
+<div class="container">
+<div class="row">
+    <center>
+<p>Copyright © 2021.E-Vendor.</p>
+    </center>
+<div class="col-sm-6"></div>
+</div>
+</div>
+</section>
+</footer>
---
+++ E_Vendor/include/header.php
@@ -0,0 +1,52 @@
+    <div class="navbar navbar-inverse navigationbar">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="navbar-header">
+                        <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                        <a href="#" class="navbar-brand">E-Vendor</a>
+                    </div>
+
+                    <div class="navbar-collapse collapse" id="mobile_menu">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Category <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">About One</a></li>
+                                    <li><a href="#">About Two</a></li>
+                                    <li><a href="#">About Three</a></li>
+                                    <li><a href="#">About Four</a></li>
+                                    <li><a href="#">About Five</a></li>
+                                    <li><a href="#">About Six</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="about_page.php">About Us</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="sell_page.php">Sell</a></li>
+                            <li><a href="contact_page.php">Contact Us</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <form action="" class="navbar-form">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <input type="search" name="search" id="" placeholder="Search Anything Here..." class="form-control">
+                                            <span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+                            <li><a href="login_signup_page.php"><span class="glyphicon glyphicon-log-in"></span> Login / Sign Up</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
